7c6183f69c89adf25cd6fdbc2461de97dba02fca
ReverseGTLT <108753232+ReverseGTLT@users.noreply.github.com>
2022-11-21 21:34:23 +0200

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@
  * @param {Object} object
  * @returns {string[]}
  */
-export const keys = (object) => {};
+export const keys = (object) => {
+  let arr = [];
+  for (let item in keys) {
+   arr.push(item)
+  }
+  return arr;
+};
 
 /**
  * Принимает объект, возвращает массив значений объекта. Также без использования
@@ -12,14 +18,28 @@ export const keys = (object) => {};
  * @param {Object} object
  * @returns {any[]}
  */
-export const values = (object) => {};
+export const values = (object) => {
+  let arr = [];
+  for (let item in values) {
+    arr.push(values[item])
+  }
+  return arr;
+};
+
 
 /**
  * Принимаем объект, возвращаем энтри. Все также без методов, используем цикл
  * @param {Object} object
  * @returns {[string, any][]}
  */
-export const entries = (object) => {};
+export const entries = (object) => {
+  let arr = [];
+  for (const item in object) {
+    let arrNew = [item, object[item]];
+    arr.push(arrNew);
+  }
+  return arr;
+};
 
 /**
  * Тут наоборот, мы должны сделать из энтри объект обратно) Можете погуглить
@@ -27,4 +47,10 @@ export const entries = (object) => {};
  * @param {[string, any][]} entries
  * @returns {Object}
  */
-export const fromEntries = (entries) => {};
+export const fromEntries = (entries) => {
+  let obj = {};
+  for (let item of entries) {
+    obj[item[0]] = item[1];
+  }
+  return obj;
+};
